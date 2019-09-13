f4bf35c1a6702f8d27a8c9421a8053ead60f1452
Tuan Truong <tuan.truong625@gmail.com>
2019-09-12 23:23:08 -0400

    Add screens for projects

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -17,7 +17,36 @@
 
     <div class="project">
       <div class="project__card">
-        <img class="project__image" src="@/assets/modeler-screen.png" alt="web">
+        <img class="project__image" src="@/assets/gym-buddy-screen.png" alt="web">
+      </div>
+      <div class="project__text">
+        <h2>Projects</h2>
+        <p class="project__text--sub-header">
+          I always love solving problems and creating things out of nothing.
+          Building web applications is the perfect medium for me. Whether you need me to design
+          and build your vision I’m your guy.
+        </p>
+        <p class="project__text--email">tuan.truong.dsgn@gmail.com</p>
+      </div>
+    </div>
+
+    <div class="project">
+      <div class="project__card">
+        <img class="project__image" src="@/assets/forgoodness-cakes-screen.png" alt="web">
+      </div>
+      <div class="project__text">
+        <h2>Projects</h2>
+        <p class="project__text--sub-header">
+          I always love solving problems and creating things out of nothing.
+          Building web applications is the perfect medium for me. Whether you need me to design
+          and build your vision I’m your guy.
+        </p>
+        <p class="project__text--email">tuan.truong.dsgn@gmail.com</p>
+      </div>
+    </div>
+    <div class="project">
+      <div class="project__card">
+        <img class="project__image" src="@/assets/nord-ui-screen.png" alt="web">
       </div>
       <div class="project__text">
         <h2>Projects</h2>
